d6f39fc9e669c90105a7bc5f8271435bcab1aa69
RoMTHoS <matheosromain@gmail.com>
2023-01-12 08:33:49 +0100

    add cors

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const data = require('./data.json');
+const data = require("./data.json");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
-app.get('/interventions', (req, res) => {
+app.get("/interventions", (req, res) => {
   res.json(data);
 });
